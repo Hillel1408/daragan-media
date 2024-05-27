6c8e7ea9424217b99527cccf910dd2d0d807b10e
Hillel1408 <etemax@bk.ru>
2024-05-27 19:02:22 +0300

    animation rotate

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
     return (
         <div className="flex items-center gap-1 text-white font-['Intro'] text-[14px] -translate-x-[5px]">
-            <img src="images/logo.svg" alt="" width="32px" height="32px" />
+            <img src="images/logo.svg" alt="" width="32px" height="32px" className="animation" />
             <span className="translate-y-[1px]">DARAGAN-MEDIA</span>
         </div>
     );
