2fbe6433a4e583f27879948e1663329cdbd9a125
Hillel1408 <etemax@bk.ru>
2024-06-01 12:56:13 +0300

    локализация

--- src/components/MyWorks/MyWorks.tsx
+++ src/components/MyWorks/MyWorks.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 import { Tabs, LandingPage, Frontend, Website } from "components";
 
 export default function MyWorks() {
     const [activeTab, setActiveTab] = useState(0);
 
-    const list = ["Вёрстка лендингов", "Вёрстка сайтов", "Frontend"];
+    const { t } = useTranslation();
+
+    const list = [t("LandingPages"), t("Websites"), "Frontend"];
 
     return (
         <div className="bg-[#111112] py-[100px]">
             <div className="container">
-                <h2 className="font-['Intro'] text-[32px] leading-[32px] text-white text-center mb-8">Мои работы</h2>
+                <h2 className="font-['Intro'] text-[32px] leading-[32px] text-white text-center mb-8">{t("MyProjects")}</h2>
                 <Tabs list={list} activeTab={activeTab} setActiveTab={setActiveTab} className="mb-[68px]" />
                 {activeTab === 0 && <LandingPage />}
                 {activeTab === 1 && <Website />}
--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -1,9 +1,11 @@
-import { Observer } from "components";
+import { useTranslation } from "react-i18next";
 
 export default function Technologies({ text, list }: { text: string; list: { text: string; icon: string; width: string; height: string }[] }) {
+    const { t } = useTranslation();
+
     return (
         <div className="border border-[#2b2b2d] rounded-[37px] py-[43px] px-[35px] bg-[rgba(35,38,57,0.1)]">
-            <p className="text-[20px] leading-[145%] text-white mb-1 text-center">Технологии</p>
+            <p className="text-[20px] leading-[145%] text-white mb-1 text-center">{t("Technologies")}</p>
             <p className="text-[17px] leading-[171%] text-white opacity-70 text-center mb-8">{text}</p>
             <ul className="text-[13px] leading-[223%] text-white flex gap-[33px] justify-center">
                 {list.map((item, index) => (
