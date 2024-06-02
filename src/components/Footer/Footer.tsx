561f17a7b6a86a2b552fc2943cf90ab300b51891
Hillel1408 <etemax@bk.ru>
2024-06-02 12:46:42 +0300

    локализация

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,9 @@
+import { useTranslation } from "react-i18next";
 import { Logo, Socials, Contacts } from "components";
 
 export default function Footer() {
+    const { t } = useTranslation();
+
     return (
         <div className="pt-[65px] pb-[100px] bg-[#18181A]">
             <div className="container">
@@ -8,12 +11,12 @@ export default function Footer() {
                 <div className="mt-10 flex justify-between items-start gap-5">
                     <Contacts className="gap-4 flex-col" />
                     <ul className="text-[18px] leading-[29px] opacity-70 text-white -translate-y-[6.5px]">
-                        <li>ИП Дараган Максим Александрович</li>
-                        <li>Завяки принимаются 7/24/365</li>
+                        <li>{t("IP")}</li>
+                        <li>{t("RequestsAccepted")}</li>
                     </ul>
                     <ul className="text-[18px] leading-[29px] opacity-70 text-white -translate-y-[6.5px]">
-                        <li>ОГРНИП: 315312300009831</li>
-                        <li>ИНН: 312335235806</li>
+                        <li>{t("OGRNIP")}: 315312300009831</li>
+                        <li>{t("INN")}: 312335235806</li>
                     </ul>
                     <Socials />
                 </div>
