7492864a34d92f55789cbc0b68d6629ca3403871
Hillel1408 <etemax@bk.ru>
2024-06-02 11:35:40 +0300

    локализация

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -1,6 +1,9 @@
+import { useTranslation } from "react-i18next";
 import { SecondaryButton, Button, Observer } from "components";
 
 export default function Reviews() {
+    const { t } = useTranslation();
+
     return (
         <div
             className="relative bg-[#111112] pb-[100px] pt-[36px] grid grid-cols-[1.07fr_0.93fr] before:content-[''] before:block before:h-[77px]
@@ -11,19 +14,19 @@ export default function Reviews() {
                 <Observer>
                     <>
                         <span className="bg-text font-['Intro'] text-[64px] uppercase">30+</span>
-                        <h2 className="text-[32px] font-['Intro'] uppercase text-white mt-[11px] mb-[28px]">положительных отзывов</h2>
+                        <h2 className="text-[32px] font-['Intro'] uppercase text-white mt-[11px] mb-[28px]">{t("HappyClients")}</h2>
                         <p className="max-w-[386px] text-[18px] leading-[161%] text-[rgba(255,255,255,0.7)]">
-                            Ознакомиться с отзывами можно на сайтах <span className="text-white">fl.ru и freelance.habr.com</span>
+                            {t("CheckOut1")} <span className="text-white">{t("CheckOut2")}</span>
                         </p>
                         <div className="mt-[46px] grid grid-cols-[195px_294px] gap-[15px]">
                             <SecondaryButton
-                                text="Отзывы на fl.ru"
+                                text={t("Reviews1")}
                                 onClick={() => {
                                     window.open("https://www.fl.ru/users/balzary/opinions/?sort=1&period=&author=0#op_head", "_blank");
                                 }}
                             />
                             <Button
-                                text="Отзывы на freelance.habr.com"
+                                text={t("Reviews2")}
                                 className="bg-button shadow-[inset_0px_0px_0px_100vw_#111112]"
                                 onClick={() => {
                                     window.open("https://freelance.habr.com/freelancers/hillel1408/opinions", "_blank");
