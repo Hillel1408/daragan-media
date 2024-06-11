6e22c3b2e07ee45683b61d59198574a54a1252da
Hillel1408 <etemax@bk.ru>
2024-06-11 14:12:25 +0300

    правки

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -20,7 +20,7 @@ export default function MobileMenu({ active, setActive }: { active: boolean; set
     return (
         <div
             className={classNames(
-                "absolute px-4 pb-[28px] pt-[13px] border duration-200 border-[#2b2b2d] rounded-[8px] right-0 top-0 bg-[#131316] z-10 w-[156px] flex flex-col translate-x-[14px] translate-y-[-10px] sm:translate-x-0 sm:translate-y-0 sm:w-full sm:top-[72px] sm:rounded-none sm:border-none sm:h-[calc(100vh-72px)] sm:bg-[#111112] sm:px-0",
+                "absolute px-4 pb-[28px] pt-[13px] border duration-200 border-[#2b2b2d] rounded-[8px] right-0 top-0 bg-[#131316] z-10 w-[156px] flex flex-col translate-x-[14px] translate-y-[-16px] sm:translate-x-0 sm:translate-y-0 sm:w-full sm:top-[72px] sm:rounded-none sm:border-none sm:h-[calc(100vh-72px)] sm:bg-[#111112] sm:px-0",
                 active ? "opacity-100 visible" : "opacity-0 invisible",
             )}
             onClick={(e) => {
@@ -37,7 +37,7 @@ export default function MobileMenu({ active, setActive }: { active: boolean; set
             </button>
             <div className="sm:bg-[#131316] sm:py-5 sm:px-4 sm:border-t sm:border-b sm:border-[#2b2b2d]">
                 <Switcher text={t("Switcher")} className="flex-row-reverse gap-3 [&>span]:text-[13px] mb-[30px] sm:flex-row sm:[&>span]:text-[16px]" />
-                <Languages className="ml-auto" />
+                <Languages className="justify-end sm:justify-start" />
             </div>
         </div>
     );
