dc8495c913cb6d04ee9f2828856a577fa7a2e8d1
Hillel1408 <etemax@bk.ru>
2024-06-15 12:35:55 +0300

    intro адаптив

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -7,19 +7,21 @@ export default function Intro() {
     return (
         <div className="bg-[#111112] overflow-hidden relative">
             <div
-                className="bg-intro pt-[122px] pb-[90px] bg-[rgba(35,38,57,0.1)] relative z-10 pl-[calc(100vw/2-560px)] pr-[calc(100vw/2-560px)]
+                className="bg-intro intro-before pt-[122px] pb-[90px] bg-[rgba(35,38,57,0.1)] relative z-10 pl-[calc(100vw/2-560px)] pr-[calc(100vw/2-560px)]
                 before:content-[''] before:block before:h-[77px] before:w-[100%] before:border-[2px] before:border-white before:rounded-tr-[24px]
                 before:border-l-0 before:border-b-0 before:absolute before:top-[61px] before:right-[calc(100vw/2-310px)] before:z-20 border border-[#2b2b2d]
                 border-l-0 border-r-0 xl:px-[28px] md:pt-[84px] md:pb-[82px] md:before:h-[64px] md:before:top-6 md:before:right-[calc(100vw/2-226px)]
-                md:bg-[length:500px]"
+                md:bg-[length:500px] sm:pt-[89px] sm:pb-[375px] sm:before:top-[41px]"
             >
-                <Observer>
+                <Observer className="sm:text-center">
                     <>
                         <h1 className="font-['Intro'] text-[55px] leading-[136%] uppercase text-white mb-5 max-w-[500px] md:text-[40px] md:mb-[6px]">
                             {t("FrontendDevelopment")}
                         </h1>
-                        <p className="text-[18px] leading-[161%] text-[rgba(255,255,255,0.7)] max-w-[450px] md:max-w-[300px]">{t("SeasonedFrontend")}</p>
-                        <SecondaryButton text={t("MyContacts")} className="w-[255px] mt-[38px] md:mt-[29px]" />
+                        <p className="text-[18px] leading-[161%] text-[rgba(255,255,255,0.7)] max-w-[450px] md:max-w-[300px] sm:max-w-[400px] sm:mx-auto">
+                            {t("SeasonedFrontend")}
+                        </p>
+                        <SecondaryButton text={t("MyContacts")} className="w-[255px] mt-[38px] md:mt-[29px] sm:mt-[21px]" />
                     </>
                 </Observer>
             </div>
