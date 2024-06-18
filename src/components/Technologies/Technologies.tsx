08459343a730917d6bc42001a810b06d2e61428e
Hillel1408 <etemax@bk.ru>
2024-06-18 15:38:31 +0300

    правки

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -4,7 +4,7 @@ export default function Technologies({ text, list }: { text: string; list: { tex
     const { t } = useTranslation();
 
     return (
-        <div className="border border-[#2b2b2d] rounded-[37px] py-[43px] px-[35px] bg-[rgba(35,38,57,0.1)] md:py-[23px] sm:-mx-[20px] sm:rounded-none sm:border-l-0 sm:border-r-0">
+        <div className="border border-[#2b2b2d] rounded-[37px] py-[43px] px-[35px] bg-[rgba(35,38,57,0.1)] md:py-[23px] sm:-mx-[20px] sm:rounded-none sm:border-l-0 sm:border-r-0 sm:px-5 sm:gap-5">
             <p className="text-[20px] leading-[145%] text-white mb-1 text-center sm:text-[18px]">{t("Technologies")}</p>
             <p className="text-[17px] leading-[171%] text-white opacity-70 text-center mb-8 sm:text-[16px] sm:mb-5">{text}</p>
             <ul className="text-[13px] leading-[223%] text-white flex gap-[33px] justify-center flex-wrap">
