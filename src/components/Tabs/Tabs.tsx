9039779c6adb064d322b8480357e204ae4928140
Hillel1408 <etemax@bk.ru>
2024-06-18 15:44:00 +0300

    правки

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -22,7 +22,7 @@ export default function Tabs({
                 {list.map((item, index) => (
                     <li
                         className={classNames(
-                            "py-2 px-8 rounded-[33px] cursor-pointer text-nowrap w-[228px] text-center sm:px-3 sm:w-[170px]",
+                            "py-2 px-8 rounded-[33px] cursor-pointer text-nowrap w-[150px] text-center sm:px-3 sm:w-[100px]",
                             activeTab === index && "bg-tab cursor-default",
                         )}
                         key={index}
