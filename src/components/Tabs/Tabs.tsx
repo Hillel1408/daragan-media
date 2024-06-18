c8e9d4ad56e88793b229b8820f5ea7d4f03b93bb
Hillel1408 <etemax@bk.ru>
2024-06-18 16:13:09 +0300

    правки

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -12,28 +12,30 @@ export default function Tabs({
     className?: string;
 }) {
     return (
-        <div className="md:-mx-[28px] md:overflow-auto sm:-mx-[20px]">
-            <ul
-                className={classNames(
-                    "text-[18px] leading-[29px] text-white flex justify-center gap-[2px] py-[10px] px-3 border-transparent bg-origin-border rounded-[33px] border w-max mx-auto bg-button shadow-[inset_0px_0px_0px_100vw_#111112] md:mx-[28px] sm:text-[16px] sm:mx-[20px]",
-                    className,
-                )}
-            >
-                {list.map((item, index) => (
-                    <li
-                        className={classNames(
-                            "py-2 px-2 rounded-[33px] cursor-pointer text-nowrap w-[228px] text-center md:w-[180px] sm:w-[147px]",
-                            activeTab === index && "bg-tab cursor-default",
-                        )}
-                        key={index}
-                        onClick={() => {
-                            setActiveTab(index);
-                        }}
-                    >
-                        {item}
-                    </li>
-                ))}
-            </ul>
+        <div className="md:-mx-[28px] sm:-mx-[20px]">
+            <div className="md:overflow-auto md:px-[28px] sm:px-[20px]">
+                <ul
+                    className={classNames(
+                        "text-[18px] leading-[29px] text-white flex flex-wrap justify-center gap-[2px] py-[10px] px-3 border-transparent bg-origin-border rounded-[33px] border w-max mx-auto bg-button shadow-[inset_0px_0px_0px_100vw_#111112] sm:text-[16px]",
+                        className,
+                    )}
+                >
+                    {list.map((item, index) => (
+                        <li
+                            className={classNames(
+                                "py-2 px-2 rounded-[33px] cursor-pointer text-nowrap w-[228px] text-center md:w-[180px] sm:w-[147px]",
+                                activeTab === index && "bg-tab cursor-default",
+                            )}
+                            key={index}
+                            onClick={() => {
+                                setActiveTab(index);
+                            }}
+                        >
+                            {item}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
