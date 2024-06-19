8e9dc11b5fbaba436cef0b723d363e946de81012
Hillel1408 <etemax@bk.ru>
2024-06-19 11:42:47 +0300

    +

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -7,7 +7,7 @@ export default function Technologies({ text, list }: { text: string; list: { tex
         <div className="border border-[#2b2b2d] rounded-[37px] py-[43px] px-[35px] bg-[rgba(35,38,57,0.1)] md:py-[23px] sm:-mx-[20px] sm:rounded-none sm:border-l-0 sm:border-r-0 sm:px-5 sm:gap-5">
             <p className="text-[20px] leading-[145%] text-white mb-1 text-center sm:text-[18px]">{t("Technologies")}</p>
             <p className="text-[17px] leading-[171%] text-white opacity-70 text-center mb-8 sm:text-[16px] sm:mb-5">{text}</p>
-            <ul className="text-[13px] leading-[223%] text-white flex gap-[33px] justify-center flex-wrap">
+            <ul className="text-[13px] leading-[223%] text-white flex gap-[32px] justify-center flex-wrap">
                 {list.map((item, index) => (
                     <li key={index} className="flex flex-col w-max items-center gap-4">
                         <div className="min-h-[35px] flex items-center">
